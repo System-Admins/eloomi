230c9e3b90b5d7ebf2552c952e30c4a7a50500a1
Alex Ø. T. Hansen <94194707+System-Admins-ath@users.noreply.github.com>
2025-04-01 10:41:31 +0200

    Update to 1.0.5
    - Fix UTF8 stuff
    - Fix import module in IDEs.
    - Other stuff.

--- src/SystemAdmins.Eloomi.psd1
+++ src/SystemAdmins.Eloomi.psd1
@@ -3,7 +3,7 @@
     RootModule           = 'SystemAdmins.Eloomi.psm1';
 
     # Version number of this module.
-    ModuleVersion        = '1.0.3';
+    ModuleVersion        = '1.0.5';
 
     # Supported PSEditions
     CompatiblePSEditions = @('Core', 'Desktop');
@@ -63,7 +63,7 @@
     # FunctionsToExport    = '*';
 
     # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
-    CmdletsToExport      = '*';
+    # CmdletsToExport      = '*';
 
     # Variables to export from this module
     # VariablesToExport    = '*';
--- src/SystemAdmins.Eloomi.psm1
+++ src/SystemAdmins.Eloomi.psm1
@@ -1,24 +1,11 @@
 # Variable for script path.
 [string]$scriptPath = '';
 
-# If we are running in VSCode, use the PSScriptRoot.
-if ($null -ne $psEditor)
-{
-    # Use info from variable psEditor.
-    $scriptPath = Split-Path -Path ($psEditor.GetEditorContext().CurrentFile.Path);
-}
-# Else if we are running in PowerShell ISE, use the $psISE.CurrentFile.
-elseif ($null -ne $psISE)
-{
-    # Use info from variable psISE.
-    $scriptPath = Split-Path -Path ($psISE.CurrentFile.FullPath);
-}
-# Else use the current working directory.
-else
-{
-    # Use the current working directory.
-    $scriptPath = $PSScriptRoot;
-}
+# Use PSScriptRoot.
+$scriptPath = Split-Path -Path $PSCommandPath -Parent;
+
+# Write to log.
+Write-Verbose -Message ("PowerShell module is located at '{0}'" -f $scriptPath);
 
 # Set script variable.
 $Script:scriptPath = $scriptPath;
@@ -38,6 +25,9 @@ $publicPs1Files = Get-ChildItem -Path $publicPath -Recurse -File -Filter *.ps1;
 $ps1Files += ($privatePs1Files).FullName;
 $ps1Files += ($publicPs1Files).FullName;
 
+# Write to log.
+Write-Verbose -Message ("Found {0} PowerShell files to dot source" -f $ps1Files.Count);
+
 # Loop through each PowerShell file.
 foreach ($ps1File in $ps1Files)
 {
@@ -52,7 +42,7 @@ foreach ($ps1File in $ps1Files)
     try
     {
         # Write to log.
-        Write-Debug -Message ("Dot sourcing the PowerShell file '{0}'" -f $ps1File);
+        Write-Verbose -Message ("Dot sourcing the PowerShell file '{0}'" -f $ps1File);
 
         # Dot source the file.
         . $ps1File;
@@ -63,9 +53,6 @@ foreach ($ps1File in $ps1Files)
         throw ("Something went wrong while importing the PowerShell file '{0}', the execption is:`r`n{1}" -f $ps1File, $_);
     }
 }
-
-# Write to log.
-Write-CustomLog -Message ("Script path is '{0}'" -f $scriptPath) -Level Verbose;
 
 # Get all the functions in the public section.
 $publicFunctions = $publicPs1Files.Basename;
@@ -85,7 +72,7 @@ $publicFunctions = $publicPs1Files.Basename;
 foreach ($exportFunction in $publicFunctions)
 {
     # Write to log.
-    Write-CustomLog -Message ("Exporting the function '{0}'" -f $exportFunction) -Level Verbose;
+    Write-Verbose -Message ("Exporting the function '{0}'" -f $exportFunction);
 }
 
 # Export functions.
